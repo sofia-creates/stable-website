ed3e26bdadf9acaea18da41f7ad6a199ce740a25
sofia-creates <sofia.ledin2@gmail.com>
2025-10-10 20:27:24 +0200

    added photos, clearing out auto created code

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,9 @@
+import imagePath from "../assets/horses-5499465_1280.jpg"
+
 const Hero = () => {
   return (
     <div>
-        <img src="" alt="" />
+        <img src={imagePath} alt="En vy av vår hage" />        
         <h1>Solvalla stall</h1>
         <h2>Ridlektioner & skogsturer</h2>
     </div>
