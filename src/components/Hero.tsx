61fff5b6d19787d1bdde8aa7bb526f808cbb5a09
sofia-creates <sofia.ledin2@gmail.com>
2025-10-11 15:55:08 +0200

    added header video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,27 @@
 import imagePath from "../assets/horses-5499465_1280.jpg"
+import heroVideo from "../assets/82211-578691551_medium.mp4"
 
 const Hero = () => {
   return (
-    <header id="hero">
-        <img src={imagePath} alt="En vy av vår hage" />        
-        <h1>Solvalla stall</h1>
-        <h2>Ridlektioner & skogsturer</h2>
+    <header 
+    id="hero" >
+        {/* <img 
+        className="heroBg" 
+        src={imagePath} 
+        alt="En vy av vår hage" />         */}
+        <video 
+        className="heroBg"
+        src={heroVideo}
+        autoPlay
+        loop
+        muted
+        playsInline
+        poster={imagePath}
+        />
+        <div className="heroText">
+          <h1>Solvalla stall</h1>
+          <h2>Ridlektioner & skogsturer</h2>
+        </div>
     </header>
   )
 }
