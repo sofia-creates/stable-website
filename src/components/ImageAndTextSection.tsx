557e54b1bf98688bf36d53b386099c99bf7db455
sofia-creates <sofia.ledin2@gmail.com>
2025-10-27 16:36:39 +0100

    playing with color etc

--- src/components/ImageAndTextSection.tsx
+++ src/components/ImageAndTextSection.tsx
@@ -3,17 +3,18 @@ import imagePath from "../assets/raphael-wicker-P6JRr7-FxLw-unsplash.jpg"
 
 interface ImageAndTextSectionProps {
   image?: string;
+  title?: string;
   textContent?: string;
 }
 
 let defaultParagraph = "Solvalla Stall är ett familjärt stall på landet där gemenskap, glädje och omtanke om hästarna står i centrum. Vi erbjuder ridlektioner för alla nivåer, härliga skogsturer i natursköna omgivningar och en trygg miljö där både ryttare och hästar trivs."
 
-const ImageAndTextSection = ({image = imagePath , textContent=defaultParagraph}:ImageAndTextSectionProps) => {
+const ImageAndTextSection = ({image = imagePath , title="Om oss", textContent=defaultParagraph}:ImageAndTextSectionProps) => {
   return (
     <section className="imageAndTextSection">
         <img src={image} alt="" />
         <div className="IATSectionContent">
-          <h3>Om oss</h3>
+          <h3>{title}</h3>
           <p>{textContent}</p>
           <Btn/>
         </div>
