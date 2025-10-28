88a602e220bce924d24a0ae2ca5f72421a1396a2
sofia-creates <sofia.ledin2@gmail.com>
2025-10-28 16:56:50 +0100

    let to const

--- src/components/ImageAndTextSection.tsx
+++ src/components/ImageAndTextSection.tsx
@@ -7,7 +7,7 @@ interface ImageAndTextSectionProps {
   textContent?: string;
 }
 
-let defaultParagraph = "Solvalla Stall är ett familjärt stall på landet där gemenskap, glädje och omtanke om hästarna står i centrum. Vi erbjuder ridlektioner för alla nivåer, härliga skogsturer i natursköna omgivningar och en trygg miljö där både ryttare och hästar trivs."
+const defaultParagraph = "Solvalla Stall är ett familjärt stall på landet där gemenskap, glädje och omtanke om hästarna står i centrum. Vi erbjuder ridlektioner för alla nivåer, härliga skogsturer i natursköna omgivningar och en trygg miljö där både ryttare och hästar trivs."
 
 const ImageAndTextSection = ({image = imagePath , title="Om oss", textContent=defaultParagraph}:ImageAndTextSectionProps) => {
   return (
