892bd6ebb6a272d994c2b60b889656a014fb89e6
sofia-creates <sofia.ledin2@gmail.com>
2026-06-30 16:19:04 +0200

    started on routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import { BrowserRouter as Router, Routes,  Route, Link} from 'react-router-dom'
+
 import './App.css'
 import HomePage from './pages/HomePage'
+import ContactPage from './pages/ContactPage'
 
 function App() {
 
   return (
-    <>
-     <HomePage/>
-    </>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/contact" element={<ContactPage/>} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,17 @@
+import Nav from "../components/Nav";
+import ImageAndTextSection from "../components/ImageAndTextSection";
+import Footer from "../components/Footer";
+
+const ContactPage = () => {
+
+  return (
+    <>
+        <Nav/>
+        <h1>Kontakta oss</h1>
+        <ImageAndTextSection/>
+        <Footer/>
+    </>
+  )
+}
+
+export default ContactPage
